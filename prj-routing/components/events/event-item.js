6906ddae781ee69afb69a085c8d2260676e7d9a3
Basliel-Amsalu <acychappiekiller@gmail.com>
2023-11-21 16:42:55 +0300

    modified

--- prj-routing/components/events/event-item.js
+++ prj-routing/components/events/event-item.js
@@ -1,5 +1,8 @@
-import Link from "next/link";
 import classes from "./event-item.module.css";
+import Button from "../UI/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
   const readableDate = new Date(props.event.date).toLocaleDateString("en-US", {
@@ -19,14 +22,21 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{props.event.title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{address}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
