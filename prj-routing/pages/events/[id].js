8cb9a90f1149c7e8666b1eaeeda97bd061cd3b12
Basliel-Amsalu <acychappiekiller@gmail.com>
2023-11-21 17:00:02 +0300

    modified

--- prj-routing/pages/events/[id].js
+++ prj-routing/pages/events/[id].js
@@ -1,8 +1,29 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+  const event = getEventById(router.query.id);
+
+  if (!event) {
+    return <div>No such event</div>;
+  }
   return (
-    <div>
-      <h1> Event detail</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 };
 
